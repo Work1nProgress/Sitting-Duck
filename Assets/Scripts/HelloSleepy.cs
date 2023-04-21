07c8ef7b9ac1bf005137500fddcfb2a8d5bf3054
WIPGameDev <WIPGameDev@gmail.com>
2023-04-21 21:30:46 +0200

    After initial commit

---
+++ Assets/Scripts/HelloSleepy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloSleepy : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello Sleepy!");
+    }
+}
