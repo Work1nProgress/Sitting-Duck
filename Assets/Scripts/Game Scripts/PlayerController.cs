a9432622e5d1e2343a13fd892241a34dfc180a20
Urban Strajnar <urban.strajnar@trickytribe.com>
2023-04-29 22:19:02 +0200

    steering like a madman

--- Assets/Scripts/Game Scripts/PlayerController.cs
+++ Assets/Scripts/Game Scripts/PlayerController.cs
@@ -6,14 +6,19 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
-    Vector2 StartPos, Steering, CurrentSteering, InputDirection;
+    Vector2 StartPos, InputDirection;
 
     Rigidbody2D body;
 
     [SerializeField]
-    float MaxAcceleration, AccelerationMultiplier, MaxSteering, SteeringMultiplier, Damping;
+    float MaxAcceleration, AccelerationMultiplier, Damping;
 
-    float currentAcceleration;
+    [SerializeField]
+    [Range(0,50f)]
+    [Tooltip("Values larger than 50 are a no no")]
+    float MaxSteering;
+
+    float angle;
     int steerDirection = 0;
     private void Awake()
     {
@@ -27,6 +32,8 @@ public class PlayerController : MonoBehaviour
         ControllerInput.Instance.OnMouseClick.RemoveListener(OnMouseClick);
     }
 
+
+
     void OnMouseClick(bool value)
     {
         if (value)
@@ -36,40 +43,24 @@ public class PlayerController : MonoBehaviour
         else
         {
             InputDirection = -(StartPos - Mouse.current.position.value).normalized * Mathf.Min((StartPos - Mouse.current.position.value).magnitude * AccelerationMultiplier, MaxAcceleration);
-            currentAcceleration = InputDirection.magnitude;
-            steerDirection = -(int)Mathf.Sign(Vector2.Dot(body.transform.right, InputDirection));
-            var angle1 = Vector2.Angle(body.transform.up, InputDirection);
-            var angle2 = Vector2.Angle(-body.transform.up, InputDirection);
-            Debug.Log($"{angle1} {angle2} {Vector2.Dot(body.transform.up, InputDirection)} {steerDirection}");
 
+            steerDirection = -(int)Mathf.Sign(Vector2.Dot(body.transform.right, InputDirection));
+          
+            angle = Vector2.Angle(body.transform.up, InputDirection);
+          
+        
+            body.velocity = Mathf.Min(InputDirection.magnitude, MaxAcceleration) * body.transform.up.normalized;
         }
 
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        float direction = Vector2.Dot(body.transform.up, InputDirection);
-        var angle = 0f;
-        if (direction > 0)
-        {
-            angle = Vector2.Angle(body.transform.up, InputDirection);
-        }
-        else
-        {
-            angle = Vector2.Angle(-body.transform.up, InputDirection);
-        }
-        //var angle1 = Vector2.Angle(body.transform.up, InputDirection);
-        //var angle2 = Vector2.Angle(-body.transform.up, InputDirection);
-        //Debug.Log($"{angle1} {angle2}");
-        //var angle = Mathf.Min(angle1, angle2);
 
-
-        //// Debug.Log($"{angle} {angle * Time.deltaTime}");
-        body.MoveRotation(Quaternion.Euler( 0, 0,transform.rotation.eulerAngles.z + steerDirection *angle * Time.deltaTime * MaxSteering));
-        var velocity = body.velocity;
-        //(body.transform.up) * Mathf.Min(currentAcceleration, MaxAcceleration);
-         body.velocity = new Vector2(velocity.x, velocity.y);
-         currentAcceleration *= Damping * Time.deltaTime;
+        var change = steerDirection * angle * Time.fixedDeltaTime * MaxSteering;
+        angle -= Mathf.Abs(change);
+        body.MoveRotation(Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + change));
+        body.velocity = body.velocity.magnitude * body.transform.up; 
     }
 
 
