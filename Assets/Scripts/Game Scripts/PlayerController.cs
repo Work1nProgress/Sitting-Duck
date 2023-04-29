33858568c24e89a2bda906d7e78b80888715036c
Urban Strajnar <urban.strajnar@trickytribe.com>
2023-04-29 17:09:18 +0200

    rotating player

--- Assets/Scripts/Game Scripts/PlayerController.cs
+++ Assets/Scripts/Game Scripts/PlayerController.cs
@@ -6,16 +6,18 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
-    Vector2 StartPos;
+    Vector2 StartPos, Steering, CurrentSteering, InputDirection;
 
-    Rigidbody2D rb2d;
+    Rigidbody2D body;
 
     [SerializeField]
-    float MaxForce, ForceMultiplier;
+    float MaxAcceleration, AccelerationMultiplier, MaxSteering, SteeringMultiplier, Damping;
 
+    float currentAcceleration;
+    int steerDirection = 0;
     private void Awake()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        body = GetComponent<Rigidbody2D>();
         ControllerInput.Instance.OnMouseClick.AddListener(OnMouseClick);
     }
    
@@ -33,13 +35,44 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
+            InputDirection = -(StartPos - Mouse.current.position.value).normalized * Mathf.Min((StartPos - Mouse.current.position.value).magnitude * AccelerationMultiplier, MaxAcceleration);
+            currentAcceleration = InputDirection.magnitude;
+            steerDirection = -(int)Mathf.Sign(Vector2.Dot(body.transform.right, InputDirection));
+            var angle1 = Vector2.Angle(body.transform.up, InputDirection);
+            var angle2 = Vector2.Angle(-body.transform.up, InputDirection);
+            Debug.Log($"{angle1} {angle2} {Vector2.Dot(body.transform.up, InputDirection)} {steerDirection}");
 
-            rb2d.AddForce(-(StartPos - Mouse.current.position.value).normalized * Mathf.Min((StartPos - Mouse.current.position.value).magnitude* ForceMultiplier, MaxForce));
         }
 
     }
 
-   
+    private void Update()
+    {
+        float direction = Vector2.Dot(body.transform.up, InputDirection);
+        var angle = 0f;
+        if (direction > 0)
+        {
+            angle = Vector2.Angle(body.transform.up, InputDirection);
+        }
+        else
+        {
+            angle = Vector2.Angle(-body.transform.up, InputDirection);
+        }
+        //var angle1 = Vector2.Angle(body.transform.up, InputDirection);
+        //var angle2 = Vector2.Angle(-body.transform.up, InputDirection);
+        //Debug.Log($"{angle1} {angle2}");
+        //var angle = Mathf.Min(angle1, angle2);
 
-    
+
+        //// Debug.Log($"{angle} {angle * Time.deltaTime}");
+        body.MoveRotation(Quaternion.Euler( 0, 0,transform.rotation.eulerAngles.z + steerDirection *angle * Time.deltaTime * MaxSteering));
+        var velocity = body.velocity;
+        //(body.transform.up) * Mathf.Min(currentAcceleration, MaxAcceleration);
+         body.velocity = new Vector2(velocity.x, velocity.y);
+         currentAcceleration *= Damping * Time.deltaTime;
+    }
+
+
+
+
 }
