4c35cd01ed4c335a615bd424bd8e2150948bb0ed
Urban Strajnar <urban.strajnar@trickytribe.com>
2023-05-01 18:52:57 +0200

    changed player search mehtod

--- Assets/Scripts/Game Scripts/EnemyControllers/CanedController.cs
+++ Assets/Scripts/Game Scripts/EnemyControllers/CanedController.cs
@@ -19,15 +19,39 @@ public class CanedController : EnemyController
         _approachPlayerState = new ApproachPlayerEnemyState(_walkSpeed, _attackPlayerRadius);
         _meleeAttackEnemyState = new MeleeAttackEnemyState(_attackWindUpTime, _attackDamage);
 
+        if (ControllerGame.Instance == null)
+        {
+            StartCoroutine(WaitForController());
+            return;
+        }
+
+        BeforeInit();
+
+    }
+
+    private void BeforeInit()
+    {
+        if (ControllerGame.Instance.Player == null)
+        {
+            StartCoroutine(WaitForPlayer());
+        }
+        else
+        {
+            Init();
+        }
+    }
+
+    private void Init()
+    {
         EnemyState.EnemyStateData stateData = new EnemyState.EnemyStateData(
-            GameObject.Find("Player").transform,
-            _rigidbody,
-            _bulletSpawner,
-            new EnemyState[1] { _meleeAttackEnemyState },
-            1,
-            true,
-            "Approach Player"
-            );
+      ControllerGame.Instance.Player.transform,
+       _rigidbody,
+       _bulletSpawner,
+       new EnemyState[1] { _meleeAttackEnemyState },
+       1,
+       true,
+       "Approach Player"
+       );
         _approachPlayerState.InitializeState(stateData);
 
         stateData.transitionStates = new EnemyState[1] { _approachPlayerState };
@@ -35,7 +59,22 @@ public class CanedController : EnemyController
         _meleeAttackEnemyState.InitializeState(stateData);
 
         _activeState = _approachPlayerState;
-
         base.Awake();
+    }
+
+    private IEnumerator WaitForPlayer()
+    {
+        yield return new WaitUntil(() => ControllerGame.Instance.Player != null);
+        Init();
+        
+
+    }
+
+    private IEnumerator WaitForController()
+    {
+        yield return new WaitUntil(() => ControllerGame.Instance != null);
+        BeforeInit();
+
+
     }
 }
