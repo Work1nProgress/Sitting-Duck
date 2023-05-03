f41b690542afd6d329430797abc9c699628de4c5
Urban Strajnar <urban.strajnar@trickytribe.com>
2023-05-03 20:33:50 +0200

    removed bullet fury

--- Assets/Plugins/Wayfarer Games/BulletFury/Common/FloatEvent.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace WayfarerGames.Common
-{
-    [CreateAssetMenu(fileName = "Scriptable Float Event", menuName = "Wayfarer Games/Scriptable Float Event")]
-    public class FloatEvent : ScriptableEvent<float>
-    { }
-}
--- Assets/Plugins/Wayfarer Games/BulletFury/Data/BulletSpawnedEvent.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine.Events;
-
-namespace BulletFury.Data
-{
-    [Serializable]
-    public class BulletSpawnedEvent : UnityEvent<int, BulletContainer>
-    {}
-}
