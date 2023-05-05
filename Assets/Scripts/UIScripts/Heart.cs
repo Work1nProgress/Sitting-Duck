7d94fec4848bd702ec4c9a4a00f4670e9193a47b
WIPGameDev <WIPGameDev@gmail.com>
2023-05-05 11:54:36 +0200

    Added the bleeding heart animation

---
+++ Assets/Scripts/UIScripts/Heart.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    [SerializeField] private GameObject _beating;
+
+    [SerializeField] private GameObject _bleeding;
+
+    private bool _isBeating = true;
+
+    public void ToggleHeart()
+    {
+        _isBeating = !_isBeating;
+        Debug.Log(_isBeating);
+        _beating.SetActive(_isBeating);
+        _bleeding.SetActive(!_isBeating);
+    }
+}
